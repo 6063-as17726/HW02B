c9b724e219809187bb3a9edc9b23433c6f4b95dc
Ayushi Shah <as17726@nyu.edu>
2023-09-16 21:45:10 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,53 @@
 function setup() {
     createCanvas(windowWidth, windowHeight); 
     background (150,150,150);
+    angleMode(DEGREES);
 }
 
 function draw() {
     stroke("white");
-fill (250,250,250);
-    rect(0,0,500,600);
+    fill (250,250,250);
+    rect(0,0,550,650);
+
+    push();
     fill("orange");
-    angleMode(degrees);
-    rotate(100);
-    rect(30,150,100,60);
-    fill("black");
-    rotate(25,10,200);
-    rect(10,200,20,200);
+    translate(220,150);
+    rotate(140);
+    rect(0,0,100,60);
+    pop();
+
+    push();
+    fill("purple");
+    translate(70,305);
+    rotate(232);
+    rect(0,0,20,220);
+    pop();
+
+    push();
+    fill("blacK");
+    translate(120,330);
+    rotate(230);
+    quad(0,0,0,80,40,80,50,0);
+    pop();
+
+    push();
+    fill("red");
+    translate(150,400);
+    rotate(235);
+    rect(0,0,60,250);
+    pop();
+
+    push();
+    strokeWeight(0);
+    fill("blacK");
+    translate(350,330);
+    rotate(235);
+    rect(0,0,60,140);
+    pop();
+
+    push();
+    fill("orange");
+    rect(300,360,150,150);
+    pop();
 
 }
